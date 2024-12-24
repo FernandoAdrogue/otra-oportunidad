7724de8ad12286ee07eae74b36853795acb6cdf8
aguslpp1 <aguslopreitto2@gmail.com>
2024-12-24 15:29:10 +0000

    primera modificacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,20 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          className="dark:invert rounded-md border border-solid border-black"
+          src="/the cat.jpg"
+          alt="Un gatito"
           width={180}
           height={38}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            The new page  {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
+              
             </code>
-            .
+            
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
@@ -31,13 +31,13 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
+              className="dark:inverse"
               src="/vercel.svg"
               alt="Vercel logomark"
-              width={20}
+              width={30}
               height={20}
             />
-            Deploy now
+            Facebook
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
